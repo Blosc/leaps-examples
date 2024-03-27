03389563969ecfd0b5f58f0ea56f827a77e35af4
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2024-03-27 17:15:49 +0100

    Use Grok function in compression program, link library
    Unfortunately the symbol cannot be resolved.

--- c-compression/compress-tomo.c
+++ c-compression/compress-tomo.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
 #include <blosc2.h>
+#include <grok.h>
 
 int main(void) {
   printf("Blosc2 %s\n", blosc2_get_version_string());
+
+  grk_cparameters grok_cparams;
+  grk_compress_set_default_params(&grok_cparams);
+  grok_cparams.cod_format = GRK_FMT_JP2;
+
   return 0;
 }
