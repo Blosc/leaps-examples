202dbf6907d0d548fb41b4fbacc0b454c32e2557
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2024-03-28 09:52:19 +0100

    Initialize blosc2 & blosc2_grok in compression program
    Enable additional libraries.  Unfortunately this does not build yet because of
    the missing `blosc2_grok.h` library and failure to link Grok symbols.

--- c-compression/compress-tomo.c
+++ c-compression/compress-tomo.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
 #include <blosc2.h>
+#include <blosc2_grok.h>
 #include <grok.h>
 
 int main(void) {
+  blosc2_init();
+  blosc2_grok_init(0, true);
+
   printf("Blosc2 %s\n", blosc2_get_version_string());
 
   grk_cparameters grok_cparams;
   grk_compress_set_default_params(&grok_cparams);
   grok_cparams.cod_format = GRK_FMT_JP2;
+
+  blosc2_grok_destroy();
+  blosc2_destroy();
 
   return 0;
 }
