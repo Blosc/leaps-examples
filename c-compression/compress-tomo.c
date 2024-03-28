c5987b5b23c1b70e5d586cbc02acb8bb7b287b58
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2024-03-28 10:38:19 +0100

    Add error control to compression program

--- c-compression/compress-tomo.c
+++ c-compression/compress-tomo.c
@@ -6,10 +6,13 @@
 #include <hdf5.h>
 
 int main(int argc, const char* argv[]) {
+  int status = EXIT_SUCCESS;
+
   // Parse command-line arguments
   if (argc < 3) {
     fprintf(stderr, "Usage: %s INPUT_HDF5 OUTPUT_HDF5\n", argv[0]);
-    exit(1);
+    status = EXIT_FAILURE;
+    goto out;
   }
 
   const char* dst_h5file_path = argv[2];
@@ -23,15 +26,23 @@ int main(int argc, const char* argv[]) {
   // Create output file
   hid_t dst_h5file_id;
   dst_h5file_id = H5Fcreate(dst_h5file_path, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
-  H5Fclose(dst_h5file_id);
+  if (dst_h5file_id < 0) {
+    status = EXIT_FAILURE;
+    goto out_uninit;
+  }
 
   grk_cparameters grok_cparams;
   grk_compress_set_default_params(&grok_cparams);
   grok_cparams.cod_format = GRK_FMT_JP2;
 
   // Cleanup
+  out_close_dstf:
+  H5Fclose(dst_h5file_id);
+
+  out_uninit:
   blosc2_grok_destroy();
   blosc2_destroy();
 
-  return 0;
+  out:
+  return status;
 }
